168bc82af801846e33ad1c0f9341ecfaf1fa6112
Giovanni Grieco <59712930+giovanni-grieco@users.noreply.github.com>
2025-08-29 13:09:47 +0200

    Add files via upload

---
+++ mod.cpp
@@ -0,0 +1,5 @@
+name = "Hatchet Studios H-60 Unrestricted Arsenal";
+picture = "logo_vtx_ca.paa";
+description = "Config patch that unrestricts the H-60 Arsenal";
+overview = "Config patch that unrestricts the H-60 Arsenal";
+author = "Henderson, D. Dog";
